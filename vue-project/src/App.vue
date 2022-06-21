6c7fe7da5b18a6c4907bd9d180160bcef1ec52d1
andronzi <andrey.radioniv.2003@gmail.com>
2022-06-21 18:27:18 +0700

    fix lint problems

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,30 +1,30 @@
 <script setup>
-  import "./assets/app.css";
-  import Navbar from "./components/Navbar.vue";
-  import Add from "./components/Add.vue";
-  import Tasks from "./components/Tasks.vue";
+import "./assets/app.css";
+import Navbar from "./components/NavComponent.vue";
+import Add from "./components/AddItem.vue";
+import Tasks from "./components/TaskItems.vue";
 </script>
 
 <template>
-    <div class="wrapper">
-      <h1>#todo</h1>
-      <Navbar />
-      <Add @updateTasks="tasks=$event" />
-      <Tasks :tasks="tasks"/>
-    </div>
+  <div class="wrapper">
+    <h1>#todo</h1>
+    <Navbar />
+    <Add @updateTasks="tasks = $event" />
+    <Tasks :tasks="tasks" />
+  </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        tasks: JSON.parse(localStorage.getItem('tasks')),
-      }
+export default {
+  data() {
+    return {
+      tasks: JSON.parse(localStorage.getItem("tasks")),
+    };
+  },
+  methods: {
+    update() {
+      this.tasks = JSON.parse(localStorage.getItem("tasks"));
     },
-    methods: {
-      update() {
-        this.tasks = JSON.parse(localStorage.getItem('tasks'))
-      }
-    }
-  }
+  },
+};
 </script>
---
+++ vue-project/src/components/TaskItems.vue
@@ -0,0 +1,14 @@
+<template>
+  <li v-for="item in tasks" v-bind:key="item.id">
+    <input type="checkbox">
+    {{ item.value }}
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    tasks: Array,
+  },
+};
+</script>
