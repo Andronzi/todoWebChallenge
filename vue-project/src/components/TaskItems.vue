515aab0d43a77df1873bbb3dcb52465651163d72
andronzi <andrey.radioniv.2003@gmail.com>
2022-06-21 18:44:07 +0700

    add toggler

--- vue-project/src/components/TaskItems.vue
+++ vue-project/src/components/TaskItems.vue
@@ -1,8 +1,10 @@
 <template>
-  <li v-for="item in tasks" v-bind:key="item.id">
-    <input type="checkbox">
-    {{ item.value }}
-  </li>
+  <div class="task-list" v-for="item in tasks" v-bind:key="item.id">
+    <input type="checkbox" :id="item.id" @click="toggle">
+    <li class="task-list__element">
+      {{ item.value }}
+    </li>
+  </div>
 </template>
 
 <script>
@@ -10,5 +12,20 @@ export default {
   props: {
     tasks: Array,
   },
+  methods: {
+    toggle(event) {
+      event.target.nextSibling.classList.toggle("line-through");
+    }
+  }
 };
 </script>
+
+<style>
+  .task-list__element {
+    margin-left: 30px;
+  }
+
+  .line-through {
+    text-decoration: line-through
+  }
+</style>
