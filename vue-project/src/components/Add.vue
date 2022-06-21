556ebcccb98ca3a64d0b033e8c5d93cadaa0c0c5
andronzi <andrey.radioniv.2003@gmail.com>
2022-06-21 18:13:06 +0700

    the addition of tasks has been added

---
+++ vue-project/src/components/Add.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <input ref="input" type="text" placeholder="add details">
+        <button @click="add">Add</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            add() {
+                let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+                tasks.push(this.$refs.input.value);
+                localStorage.setItem("tasks", JSON.stringify(tasks));
+                this.$emit('updateTasks', tasks)
+            }
+        }
+    }
+</script>
